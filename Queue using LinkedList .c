55f68f02ac6bbd82b47a5eb223ee7ec3d77a6815
Nimesh <nimesh17@Nimeshs-Macbook-Pro.local>
2017-09-17 12:50:01 +0530

    CODE refactor

--- Queue using LinkedList .c
+++ Queue using LinkedList .c
@@ -1,46 +1,9 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include "functions.h"
 
-struct Node{
-  int data;
-  struct Node* next;
-};
-typedef struct Node* node;
 node head,tail;
-node createNewNode(){
-  return (node)malloc(sizeof(node));
-}
-void enqueue2(int x){
-  node current = createNewNode();
-  current->data = x;
-  current->next = head;
-  head = current;
-}
 
-void enqueue(int x){
-  if(head == NULL){
-    enqueue2(x);
-  }
-  else{
-    node temp1 = head;
-    while(temp1->next != NULL){
-      temp1 = temp1->next;
-    }
-    node temp2 = createNewNode();
-    temp2->data = x;
-    temp2->next = NULL;
-    temp1->next = temp2;
-  }
-}
-
-void print(){
-  node temp = head;
-  while(temp != NULL){
-    printf("%d ",temp->data);
-    temp = temp->next;
-  }
-  printf("\n");
-}
 void dequeue(){
   node temp = head;
   if(temp == NULL) printf("Queue is empty\n");
@@ -70,10 +33,11 @@ int main(){
     scanf("%d",&ch);
     switch(ch){
       case 1: scanf("%d",&n);
-              enqueue(n);
+              insertAtEnd(n);
               print();
               break;
-      case 2: dequeue();
+      case 2: deleteAtBeginning();
+              print();
               break;
       case 3: printf("Head points to %d\n",getHead());
               printf("Tail points to %d\n",getTail());
